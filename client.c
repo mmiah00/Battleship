25e733bbb104797a192316cb7b2098db3cd22282
mmiah00 <mmiah00@stuy.edu>
2020-01-10 02:24:11 +0000

    wrote code that keeps track of the coordinates that the client enters

--- client.c
+++ client.c
@@ -1,12 +1,40 @@
 #include "networking.h"
 
-int main(int argc, char **argv) {
+struct coordinate {
+  int x,y;
+};
 
+struct coordinate spots_hit [100]; //coordinates you have attacked on the other team
+int i; //to keep track of the index of spots_hit
+
+struct coordinate makecor (char *str) {
+  struct coordinate cor;
+  char * dup = strdup (str);
+  int i = 0;
+  while (i < 2) {
+    str = strsep (&dup, " ");
+    if (i == 0) {
+      cor.x = atoi (str);
+    }
+    else {
+      cor.y = atoi (str);
+    }
+    i ++;
+  }
+  return cor;
+}
+
+void addcor (struct coordinate c) {
+  spots_hit[i] = c;
+  i ++;
+}
+
+int main(int argc, char **argv) {
   int server_socket;
   char buffer[BUFFER_SIZE];
-
+  i = 0;
   if (argc == 2)
-    server_socket =g client_setup( argv[1]);
+    server_socket = client_setup( argv[1]);
   else
     server_socket = client_setup( TEST_IP );
 
@@ -16,6 +44,8 @@ int main(int argc, char **argv) {
     *strchr(buffer, '\n') = 0;
     write(server_socket, buffer, sizeof(buffer));
     read(server_socket, buffer, sizeof(buffer));
+    struct coordinate c = makecor (buffer);
+    addcor (c);
     printf("received: [%s]\n", buffer);
   }
 }
